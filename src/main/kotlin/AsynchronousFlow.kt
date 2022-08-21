1b593d1898f1e7b87e246aa7a58d4345da995ca9
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-08-21 21:47:52 +0900

    Creating Flow

--- src/main/kotlin/AsynchronousFlow.kt
+++ src/main/kotlin/AsynchronousFlow.kt
@@ -10,6 +10,22 @@ fun main() {
             println("Received prime number $it")
         }
         println("Finished receiving numbers")
+        /*
+        Messaging prime number
+        Received prime number 2
+        Received prime number 3
+        Received prime number 5
+        Received prime number 7
+        Received prime number 11
+        Received prime number 13
+        Received prime number 17
+        Received prime number 19
+        Received prime number 23
+        Received prime number 29
+        Finished receiving numbers
+
+        Process finished with exit code 0
+         */
     }
 
 }
